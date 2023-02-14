190a6db9aab878ed1e71676569e7495b13d71424
Christian Kulat <cdkulat1@gmail.com>
2023-02-14 16:36:05 -0500

    Starting to load projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,42 +2,43 @@
 // Note: Here we are getting data from a js file, but in a different project I'll be fetching these projects from some srt of APIs.
 
 const projects = [
-	{
+	//Formatting guide:
+
+	/*{
 		id: 1,
 		title: 'Google Health Platform',
 		category: 'Web Application',
 		img: require('@/assets/images/web-project-2.jpg'),
-	},
+	}, */
+
 	{
-		id: 2,
-		title: 'Phoenix Digital Agency',
-		category: 'Mobile Application',
-		img: require('@/assets/images/mobile-project-2.jpg'),
+		id:1,
+		title:'Number Guessing Game',
+		category: '',
+		img:'',
 	},
+
 	{
-		id: 3,
-		title: 'Project Management UI',
-		category: 'UI/UX Design',
-		img: require('@/assets/images/ui-project-1.jpg'),
+		id:2,
+		title:"Dice Rolling Discord Bot in Go",
+		category:'',
+		img:'',
 	},
+
 	{
-		id: 4,
-		title: 'Cloud Storage Platform',
-		category: 'UI/UX Design',
-		img: require('@/assets/images/ui-project-2.jpg'),
+		id:3,
+		title:"PDF Text Extractor",
+		category:'',
+		img:'',
 	},
+
 	{
-		id: 5,
-		title: 'React Social App',
-		category: 'Mobile Application',
-		img: require('@/assets/images/mobile-project-1.jpg'),
-	},
-	{
-		id: 6,
-		title: 'Apple Design System',
-		category: 'Web Application',
-		img: require('@/assets/images/web-project-1.jpg'),
-	},
+		id:4,
+		title:"Urban Definitions",
+		category:'',
+		img:''
+	}
+
 ];
 
 export default projects;
