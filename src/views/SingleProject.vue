03b78493b9e4022538c11adf771fda59d4e0478f
Christian Kulat <cdkulat1@gmail.com>
2023-02-16 13:52:34 -0500

    Restoring project pages so they aren't empty - need to alias data from pID

--- src/views/SingleProject.vue
+++ src/views/SingleProject.vue
@@ -3,7 +3,6 @@ import feather from 'feather-icons';
 import ProjectHeader from '../components/projects/ProjectHeader.vue';
 import ProjectGallery from '../components/projects/ProjectGallery.vue';
 import ProjectInfo from '../components/projects/ProjectInfo.vue';
-import projects from '../data/projects';
 
 export default {
 	name: 'Projects',
@@ -14,7 +13,52 @@ export default {
 	},
 	data: () => {
 		return {
-			projectItems: projects
+			singleProjectHeader: {
+				singleProjectTitle: 'UrbanDef',
+				singleProjectDate: 'Jul 26, 2021',
+				singleProjectTag: 'UI / Frontend',
+			},
+			projectInfo: {
+				objectivesHeading: 'Objective',
+				objectivesDetails:
+					'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, natus! Quibusdam enim quod in esse, mollitia molestias incidunt quas ipsa accusamus veniam.',
+				technologies: [
+					{
+						title: 'Tools & Technologies',
+						techs: [
+							'HTML',
+							'CSS',
+							'JavaScript',
+							'Vue.js',
+							'TailwindCSS',
+							'AdobeXD',
+						],
+					},
+				],
+				projectDetailsHeading: 'Project Details',
+				projectDetails: [
+					{
+						id: 1,
+						details:
+							'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.',
+					},
+					{
+						id: 2,
+						details:
+							'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?',
+					},
+					{
+						id: 3,
+						details:
+							'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?',
+					},
+					{
+						id: 4,
+						details:
+							'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.',
+					},
+				],
+			},
 		};
 	},
 	mounted() {
@@ -38,7 +82,6 @@ export default {
 		<!-- Project information -->
 		<ProjectInfo :projectInfo="projectInfo" />
 
-		<!-- Project related projects -->
 	</div>
 </template>
 
